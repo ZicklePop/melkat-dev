65990deb840adc61b7f4e33397034e4a6b643a14
ZicklePop <ZicklePop@users.noreply.github.com>
2019-08-18 10:40:12 -0700

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+# [melkat.dev](https://melkat.dev)
+
+A staticly exported [Next](https://github.com/zeit/next.js/) project
+made with [Glamor and
+Tachyons](https://github.com/wookiehangover/glamor-tachyons/)
+
---
+++ pages/index.js
@@ -0,0 +1,66 @@
+import React from 'react'
+import Head from 'next/head'
+import { insertRule, css, style, rehydrate } from 'glamor'
+import { tachyons, reset, wrap } from 'glamor-tachyons'
+
+if (typeof window !== 'undefined') {
+  rehydrate(window.__NEXT_DATA__.ids)
+}
+reset({ insertRule })
+const t = (classNames) => css(tachyons(classNames))
+
+const styles = {
+  ats: style({
+    '&::after': {
+      content: 'ats',
+      color: '#AAA'
+    }
+  }),
+  green: style({ color: '#69b200' }),
+  blue: style({ color: '#00aeff' }),
+  flexGrow: style({ webkitBoxFlex: 1, msFlexPositive: 1, flexGrow: 1 })
+}
+
+const classNames = wrap({
+  main: 'mw8 center sans-serif near-black f3 fw2',
+  h1: 'lh-title fw2 f2 pa3',
+  a: 'link',
+  articleList: 'flex-ns flex-wrap-ns',
+  firstArticleContainer: 'w-100 dib flex-ns ph3 pv2',
+  articleContainer: 'w-100 w-50-ns dib flex-ns ph3 pv2',
+  article: 'w-100 db ba br2 b--black-10 mb3 overflow-hidden no-underline',
+  articleImageDiv: 'w-100 aspect-ratio aspect-ratio--16x9',
+  articleImage: 'aspect-ratio--object cover bg-center',
+  articleDetailsDiv: 'pa3',
+  articleTitle: 'link lh-title f4 fw3 mv0',
+  articleDate: 'gray db pv2 f6'
+}, css)
+
+export default () => (
+  <main className={classNames.main}>
+    <Head>
+      <title>melkat.dev</title>
+      <meta name='twitter:title' content='melkat.dev' />
+      <meta name='twitter:description' content='melkat codes' />
+      <meta name='twitter:image' content='https://melkat.dev/static/facebook-open-graph.png' />
+      <meta name='twitter:image:alt' content='logo for melkat.dev' />
+      <meta property='og:description' content='melkat.dev' />
+      <meta property='og:image' content='https://melkat.dev/static/facebook-open-graph.png' />
+      <meta property='og:image:height' content='630' />
+      <meta property='og:image:width' content='1200' />
+      <meta property='og:title' content='melkat.dev' />
+    </Head>
+    <h1 className={classNames.h1}>
+      <a
+        {...styles.ats}
+        className={t('no-underline near-black')}
+        href='/'
+      >
+        {'melkat.dev'}
+      </a>
+    </h1>
+    <div className={classNames.articleList}>
+    </div>
+    </div>
+  </main>
+)
