f3ef193b963ef3f407ed5eb51c2ec518e6ce8e4d
Melanie Kat <318003+ZicklePop@users.noreply.github.com>
2019-08-18 10:40:12 -0700

    init

---
+++ next.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  exportPathMap: function () {
+    return {
+      '/': { page: '/' },
+      '/bad-actors': { page: '/bad-actors' },
+      '/blow-fish': { page: '/blow-fish' },
+      '/comix-experience': { page: '/comix-experience' },
+      '/comixology-to-kindle': { page: '/comixology-to-kindle' },
+      '/destruction-of-github': { page: '/destruction-of-github' },
+      '/dont-like-your-company': { page: '/dont-like-your-company' },
+      '/how-to-san-francisco': { page: '/how-to-san-francisco' },
+      '/nintendo-switch-change': { page: '/nintendo-switch-change' },
+      '/request-to-mailchimp': { page: '/request-to-mailchimp' },
+      '/waiting-on-apple-watch': { page: '/waiting-on-apple-watch' }
+    }
+  }
+}
